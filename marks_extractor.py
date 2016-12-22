72e977342f0c8c82a76dc89a04d8bdf5b5eb0a39
blakeyp <blakeyp@hotmail.co.uk>
2016-12-22 18:18:52 +0000

    added a few edge cases - needs tidying up

--- marks_extractor.py
+++ marks_extractor.py
@@ -1,13 +1,19 @@
 import re, os, sys
 
-def main(paper):
+def main():
+
+	global marks2style
+
+	paper = sys.argv[1]
 
 	q_marks = []   # list of lists one for each question with the num marks for each of its parts
 
 	for file in os.listdir('../questions'):   # traverse text files of questions
+		
+		marks2style = False
 
 		if file.endswith('.txt'):
-			if file.endswith('0.txt'):
+			if file.endswith('0.txt') or file.endswith('rubric.txt'):
 				continue
 			f = open('../questions/'+file,'rU')
 			q_text = f.readlines()   # string containing question text
@@ -62,13 +68,20 @@ def main(paper):
 
 def find_marks(text):   # find the number of marks for a question part
 	#print 'PART:', part
+	global marks2style
 	marks=0
 	for line in text:   # traverse text lines of part
-	 	marks_regex = re.compile(r'.*\[([0-9]{1,2})\]\s*$')
-	 	marks_match = marks_regex.match(line)
-		if marks_match:   # if find number of marks e.g. [5]
+	 	marks1_regex = re.compile(r'.*\[([0-9]{1,2})(?:\s?marks)?\]\s*$')
+	 	marks1_match = marks1_regex.match(line)
+	 	marks2_regex = re.compile(r'.*\(([0-9]{1,2})\s?marks\).*$')
+	 	marks2_match = marks2_regex.match(line)
+		if not marks2style and marks1_match:   # if find number of marks e.g. [5]
 	 		# add to marks; add up all marks from 'subparts' of a part
-			marks = marks+int(marks_match.group(1))
+			marks = marks+int(marks1_match.group(1))
+		elif marks2_match:
+			print 'yes'
+			marks = marks+int(marks2_match.group(1))
+			marks2style = True
 			#print line, marks
 	#print 'MARKS:', marks; print
 	return marks
